bd7ad8e58919b51ceaee6f8ab313ca2bad100a7e
akshat <mishraakshat2610@gmail.com>
2025-12-01 21:19:50 +0530

    initial code on dev branch

---
+++ src/main/java/com/parent/am/AimDm/AimDmApplication.java
@@ -0,0 +1,13 @@
+package com.parent.am.AimDm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimDmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimDmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parent/am/AimDm/AimDmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parent.am.AimDm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimDmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
